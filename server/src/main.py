9c341a68935d55c5dfd1280c1d33394f533ba0d7
dipto0321 <diptokmk47@gmail.com>
2022-02-10 22:49:21 +0600

    :sparkles: Questions endpoint created

--- server/src/main.py
+++ server/src/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+from typing import Any, Dict, List
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from src.questions import questions
 
 description = """
 Personality test API helps you to generate your personality. 🚀
@@ -29,6 +33,28 @@ app.add_middleware(
 )
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 def read_welcome():
     return {"message": "Welcome to personality test API"}
+
+
+@app.get("/questions", status_code=status.HTTP_200_OK)
+def get_question():
+    questions_data = jsonable_encoder(questions)
+    return JSONResponse(
+        content={"data": questions_data, "total_questions": len(questions_data)}
+    )
+
+
+@app.get("/questions/<:id>", status_code=status.HTTP_200_OK)
+def get_question_by_id(id: int):
+    res = {}
+    question_length = len(questions)
+    if id <= question_length and id > 0:
+        question = questions[id - 1]
+        questions_data = jsonable_encoder(question)
+        res["data"] = questions_data
+    else:
+        res["data"] = []
+
+    return JSONResponse(content=res)
